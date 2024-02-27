af88a15fc13361fa5f810e35e8dc2ac2057789d9
smntsng <144932270+smntsng@users.noreply.github.com>
2024-02-27 22:19:06 +0000

    updated project styling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Projects.css';
 
 const Projects = () => {
+    const projectsRef = useRef(null);
+
+    const handleScroll = (event) => {
+        const container = projectsRef.current;
+        const delta = Math.max(-1, Math.min(1, (event.deltaY || -event.detail)));
+        container.scrollLeft += delta * 100; // Adjust scroll speed as needed
+    };
+
     return (
-        <div className="projects-container">
+        <div className="projects-container" ref={projectsRef} onWheel={handleScroll}>
             <div className="projects-wrapper">
                 <div className="project-item">
                     <img src="src\assets\images\Website screenshot\Sneakerstreets screen shot.jpg" alt="Project 1" />
@@ -12,7 +20,20 @@ const Projects = () => {
                         <p>Description of Project 1</p>
                     </div>
                 </div>
-                {/* Add more project items as needed */}
+                <div className="project-item">
+                    <img src="src\assets\images\Website screenshot\Sneakerstreets screen shot.jpg" alt="Project 1" />
+                    <div className="project-details">
+                        <h3>Project 1</h3>
+                        <p>Description of Project 1</p>
+                    </div>
+                </div>
+                <div className="project-item">
+                    <img src="src\assets\images\Website screenshot\Sneakerstreets screen shot.jpg" alt="Project 1" />
+                    <div className="project-details">
+                        <h3>Project 1</h3>
+                        <p>Description of Project 1</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
