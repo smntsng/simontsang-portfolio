c4fd294f33f1cd6abbd98d36d17affbfb4bc605e
smntsng <144932270+smntsng@users.noreply.github.com>
2024-03-08 13:35:08 +0000

    updated project photos

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,6 +2,11 @@ import React, { useRef, useState } from 'react';
 import './Projects.css';
 // images
 import sneakerstreetscreenshot1 from "../../assets/images/sneakerstreetsscreenshot.jpg"
+import sneakerstreetscreenshot2 from "../../assets/images/sneakerstreetsscreenshot2.jpg"
+import harmonyhealscreenshot from "../../assets/images/harmonyhealscreenshot.jpg"
+import harmonyhealscreenshot2 from "../../assets/images/harmonyhealscreenshot2.jpg"
+import labcloscreenshot from "../../assets/images/labcloscreenshot.jpg"
+import labcloscreenshot2 from "../../assets/images/labcloscreenshot2.jpg"
 
 
 const Projects = () => {
@@ -14,7 +19,7 @@ const Projects = () => {
             <div className="projects-wrapper">
                 <div className="project-item">
                     <img className="display-image" src={sneakerstreetscreenshot1} alt="Sneaker Streets Project" />
-                    <img className="hover-image" src="src/assets/images/Website screenshot/Sneakerstreets screen shot 2.jpg" alt="Hover Image" />
+                    <img className="hover-image" src={sneakerstreetscreenshot2} alt="Hover Image" />
                     <div className="project-details">
                         <h3>SneakerStreets Ecommerce Store</h3>
                         <p>SneakStreets is an ecommerce store and news aggregator for trending sneakers. It was built as a collaborative project to highlight use of React to build a web application.
@@ -31,8 +36,8 @@ const Projects = () => {
                     </div>
                 </div>
                 <div className="project-item">
-                    <img className="display-image" src="src\assets\images\Website screenshot\Harmony Heal screen shot.jpg" alt="Sneaker Streets Project" />
-                    <img className="hover-image" src="src\assets\images\Website screenshot\Harmony Heal screen shot 2.jpg" alt="Hover Image" />
+                    <img className="display-image" src={harmonyhealscreenshot} alt="Sneaker Streets Project" />
+                    <img className="hover-image" src={harmonyhealscreenshot2} alt="Hover Image" />
                     <div className="project-details">
                         <h3>Harmony Heal</h3>
                         <p>The website has been developed with wellbeing in mind and aims to be a simple to use tool that can be accessed by carers or users who want to listen to different music playlists depending on how they are feeling.
@@ -48,8 +53,8 @@ const Projects = () => {
                 </div>
 
                 <div className="project-item">
-                    <img className="display-image" src="src\assets\images\Website screenshot\Labclo screen shot.jpg" alt="Sneaker Streets Project" />
-                    <img className="hover-image" src="src\assets\images\Website screenshot\Labclo screen shot 2.jpg" alt="Hover Image" />
+                    <img className="display-image" src={labcloscreenshot} alt="Sneaker Streets Project" />
+                    <img className="hover-image" src={labcloscreenshot2} alt="Hover Image" />
                     <div className="project-details">
                         <h3>Labclo Ecommerce Store</h3>
                         <p>Labclo is a live Ecommerce store built using Wix with an inventory of over 600 items
