a491d0f9ccdfd92d10abf2db79ff6e54cff96b5d
smntsng <144932270+smntsng@users.noreply.github.com>
2024-04-10 11:06:30 +0100

    update logo with link and postitioning

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ import logo from "../../assets/images/logo.png"
 const Header = () => {
     return(
         <div className='logo'>
+             <a href="https://simontsang-portfolio.netlify.app/"></a>
             <img src={logo} alt="simon tsang logo"></img>
+            </a>
         </div>
     )
 }
